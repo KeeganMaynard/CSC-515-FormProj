a140661d9d62974cc9c0ccefa60f1d906ab9e08a
Keegan <kgm1328@live.missouristate.edu>
2022-05-03 19:50:39 -0500

    JS validation and started PHP validation

--- sub_page.php
+++ sub_page.php
@@ -1,3 +1,17 @@
+<html>
+<body>
+
 <?php
-//PHP Code
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$email = $_POST["email"];
+$mobile = $_POST["mobile"];
+$gender = $_POST["gender"];
+$city = $_POST["city"];
+$state = $_POST["state"];
+$qualif = $_POST["qualif"];
+$pass = $_POST["pword"];
 ?>
+
+</body>
+</html>
