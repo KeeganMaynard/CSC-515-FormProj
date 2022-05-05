234ae57d49cb5f757470285300d997b8fd77dfbb
Sam <smcarthur000@gmail.com>
2022-05-04 23:31:50 -0500

    Added AJAX call, php db connection, php sql query

--- sub_page.php
+++ sub_page.php
@@ -1,17 +1,23 @@
-<html>
-<body>
-
 <?php
-$fname = $_POST["fname"];
-$lname = $_POST["lname"];
-$email = $_POST["email"];
-$mobile = $_POST["mobile"];
-$gender = $_POST["gender"];
-$city = $_POST["city"];
-$state = $_POST["state"];
-$qualif = $_POST["qualif"];
-$pass = $_POST["pword"];
-?>
+    require_once "db.php";
 
-</body>
-</html>
+$fname = mysqli_real_escape_string($conn,$_POST["firstname"]);
+$lname = mysqli_real_escape_string($conn,$_POST["lastname"]);
+$email = mysqli_real_escape_string($conn,$_POST["email"]);
+$mobile = mysqli_real_escape_string($conn,$_POST["mobile"]);
+$gender = mysqli_real_escape_string($conn,$_POST["gender"]);
+$city = mysqli_real_escape_string($conn,$_POST["city"]);
+$state = mysqli_real_escape_string($conn,$_POST["state"]);
+$qualif = mysqli_real_escape_string($conn,$_POST["qualification"]);
+$pass = mysqli_real_escape_string($conn,$_POST["password"]);
+
+if (mysqli_query($conn,"INSERT INTO csc515-project (firstname, lastname, email, mobile, gender,
+ city, state, qualification, password)
+VALUES ('" . $fname . "', '" . $lname. "', '" . $email . "', '" . $mobile . "', '"  . $gender . "', '" . $city . "', '" . $state . "', '" . $qualif . "', '" . $pass . "')")){
+    echo '1';
+} else {
+    echo "Error: " . $sql . "" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
