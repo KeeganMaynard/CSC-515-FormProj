b7d1f68c42e720dedf2ef578d0c64362343dc212
Keegan <kgm1328@live.missouristate.edu>
2022-05-08 13:46:12 -0500

    Create table if it doesn't already exist

--- db.php
+++ db.php
@@ -8,4 +8,33 @@ if(!$conn)
 {
 	die('Could not Connect MySQL Server:' .mysql_error());
 }
+
+$check = "SHOW TABLES LIKE 'member_registration'";
+
+if(mysqli_query($conn, $check))
+{
+	//Table already created - do nothing
+}
+else
+{
+	//sql to create table
+	$sql = "CREATE TABLE member_registration(
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(255) NOT NULL,
+	lastname VARCHAR(255) NOT NULL,
+	email VARCHAR(255),
+	mobile INT(20),
+	gender VARCHAR(255),
+	city VARCHAR(255),
+	state VARCHAR(255),
+	qualification VARCHAR(255),
+	password VARCHAR(255)
+	)";
+	
+	if(!mysqli_query($conn, $sql))
+	{
+		echo "Error creating table: ".mysqli_error($conn);
+	}
+}
+
 ?>
