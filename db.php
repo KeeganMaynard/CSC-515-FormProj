fdbd891c7bdeb3ed356eb9166501280d7443f2b3
Keegan <kgm1328@live.missouristate.edu>
2022-05-07 20:11:33 -0500

    file to connect to database

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = 'root';
+$password = '';
+$dbname = "mysql";
+$conn = mysqli_connect($servername, $username, $password, "$dbname");
+if(!$conn)
+{
+	die('Could not Connect MySQL Server:' .mysql_error());
+}
+?>
